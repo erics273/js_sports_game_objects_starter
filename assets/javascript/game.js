36b451cb5d89691361502d271302c15ad5c4fd3b
Eric Schwartz <eric@kenzie.academy>
2019-10-16 23:57:40 -0400

    initial commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+(function () {
+
+  //DOM Elements for team 1
+  let teamOneNameElement = document.querySelector("#teamone-name");
+  let teamOneShootButtonElement = document.querySelector("#teamone-shoot");
+  let teamOneGoalCountElement = document.querySelector("#teamone-numhits");
+  let teamOneShotCountElement = document.querySelector("#teamone-numshots");
+
+  //DOM Elements for team 2
+  let teamTwoNameElement = document.querySelector("#teamtwo-name");
+  let teamTwoShootButtonElement = document.querySelector("#teamtwo-shoot");
+  let teamTwoGoalCountElement = document.querySelector("#teamtwo-numhits");
+  let teamTwoShotCountElement = document.querySelector("#teamtwo-numshots");
+
+  //DOM Elements for reset functions
+  let resetButtonElement = document.querySelector("#reset");
+  let resetCountElement = document.querySelector("#num-resets");
+
+})();
